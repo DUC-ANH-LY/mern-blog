4419b789b9b570795fbf8913919a7d25d6412e3e
ducanhly <namobilegame@gmail.com>
2023-09-17 23:52:33 +0700

    create header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-red-400">Hello world</h1>
+     <HomePage/>
     </div>
   );
 }
